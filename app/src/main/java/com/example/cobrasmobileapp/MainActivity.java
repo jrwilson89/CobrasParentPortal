c389c85c5d8960b5e91f124674f611ac41ddf736
Jason Wilson <jrwilson89@gmail.com>
2023-12-30 13:27:44 -0600

    Added drop-down box for add_child page in MainActivity.

--- app/src/main/java/com/example/cobrasmobileapp/MainActivity.java
+++ app/src/main/java/com/example/cobrasmobileapp/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.cobrasmobileapp;
 
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] item = {"Cheer", "Football"};
+    AutoCompleteTextView autoCompleteTextView;
+    ArrayAdapter<String> adapterItems;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.initial_page);
+
+        autoCompleteTextView = findViewById(R.id.auto_complete_txt);
+        adapterItems = new ArrayAdapter<String>(this, R.layout.list_items, item);
+        autoCompleteTextView.setAdapter(adapterItems);
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(MainActivity.this, "Item: " + item, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
